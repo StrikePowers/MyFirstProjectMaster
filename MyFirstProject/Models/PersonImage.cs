8e4f665440770b50573741ee6bca432a26c5fdb0
StrikePowers <teherror404@gmail.com>
2021-12-02 10:42:59 -0600

    asdfasdfasfdYo MOMMMA

--- MyFirstProject/Models/PersonImage.cs
+++ MyFirstProject/Models/PersonImage.cs
@@ -21,9 +21,9 @@ namespace MyFirstProject.Models
         {
             return new List<PersonImage>
             {
-                new PersonImage("A", "https://static1.colliderimages.com/wordpress/wp-content/uploads/guardians-of-the-galaxy-chris-pratt-slice.jpg?q=50&fit=contain&w=750&h=375&dpr=1.5", "r"),
-                new PersonImage("B", "https://cdn.vox-cdn.com/thumbor/-fJVqBRZRtmIlnqABk0aXyH14gs=/78x0:872x529/920x613/filters:focal(78x0:872x529):format(webp)/cdn.vox-cdn.com/assets/3755363/Groot.jpg","r"),
-                new PersonImage("C", "https://static.independent.co.uk/2020/12/24/08/bradley-cooper-rocket.jpg?width=1368&auto=webp&quality=75", "r")
+                new PersonImage("Christ Pratt", "https://static1.colliderimages.com/wordpress/wp-content/uploads/guardians-of-the-galaxy-chris-pratt-slice.jpg?q=50&fit=contain&w=750&h=375&dpr=1.5", "Star Lord"),
+                new PersonImage("Vin Diesal", "https://cdn.vox-cdn.com/thumbor/-fJVqBRZRtmIlnqABk0aXyH14gs=/78x0:872x529/920x613/filters:focal(78x0:872x529):format(webp)/cdn.vox-cdn.com/assets/3755363/Groot.jpg","Groot"),
+                new PersonImage("Bradly Cooper", "https://static.independent.co.uk/2020/12/24/08/bradley-cooper-rocket.jpg?width=1368&auto=webp&quality=75", "Rocket")
             };
         }
     }
